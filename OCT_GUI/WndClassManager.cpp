5b8c65dfdf7c0dc953419b99db001741bfcb39d0
车文竣 <94781643+ourcwj@users.noreply.github.com>
2022-12-11 08:49:47 +0800

    添加一个新类(未完工)
    添加了管理wndclass的类

---
+++ OCT_GUI/WndClassManager.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "WndClassManager.h"
+
+WndClassManager::WndClassManager()
+{
+	// 未完工代码
+
+	WndClass_list = new std::map<int, WNDCLASSEXA*>;
+	WNDCLASSEXA* wc = new WNDCLASSEXA;
+	WndClass_list->insert(std::pair<int, WNDCLASSEXA*>(0, wc));
+	wnd_it it = WndClass_list->find(0);
+	it->second->style = CS_HREDRAW | CS_VREDRAW;
+
+
+}
+
+WndClassManager::~WndClassManager()
+{
+	delete WndClass_list;
+}
---
+++ OCT_GUI/WndClassManager.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include<map>
+
+// 未完工代码
+
+class WndClassManager
+{
+public:
+	WndClassManager();
+	~WndClassManager();
+
+
+	std::map<int, WNDCLASSEXA*>* WndClass_list;
+
+private:
+	typedef std::map<int, WNDCLASSEXA*>::iterator wnd_it;
+
+};
+
